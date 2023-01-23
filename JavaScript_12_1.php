b20cfbb9fc44f36dbb2fbcd1bea4b2f36567cbf3
Sasza44 <110614574+Sasza44@users.noreply.github.com>
2023-01-23 05:16:02 +0200

    Delete JavaScript_12_1.php

--- JavaScript_12_1.php
+++
@@ -1,8 +0,0 @@
-<?php
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$details = $_POST['details'];
-
-mail('philipsevene@gmail.com', $name, $phone, $email, $details);
-?>
